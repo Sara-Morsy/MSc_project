42648cae5d60a73b6d8ab8a1c56b4d0531515810
Sara-Morsy <121258027+Sara-Morsy@users.noreply.github.com>
2025-07-17 17:43:12 +0100

    Update 2_trim.sh

--- RNAseq_code/Single_end RNAseq/2_trim.sh
+++ RNAseq_code/Single_end RNAseq/2_trim.sh
@@ -14,13 +14,13 @@ mkdir -p "$OUT_DIR"
 # Fastp processing function for single-end data
 run_fastp_single() {
     FILE="$1"
-    SAMPLE=$(basename "$FILE" .fastq)
+    SAMPLE=$(basename "$FILE" .fastq.gz)
 
     echo "Processing $SAMPLE"
 #you can add arguments for trimming here
     fastp \
         -i "$FILE" \
-        -o "$OUT_DIR/${SAMPLE}_clean.fastq" \
+        -o "$OUT_DIR/${SAMPLE}_clean.fastq.gz" \
         --detect_adapter_for_pe \
         --qualified_quality_phred 28 \
         --length_required 20 \
@@ -32,7 +32,7 @@ export -f run_fastp_single
 export IN_DIR OUT_DIR THREADS_PER_JOB
 
 # Run in parallel on all .fastq.gz files in the input directory
-find "$IN_DIR" -maxdepth 1 -name "*.fastq" -print0 \
+find "$IN_DIR" -maxdepth 1 -name "*.fastq.gz" -print0 \
   | sort -z | parallel -0 -j "$MAX_JOBS" run_fastp_single {}
 
 echo "All single-end FASTQ files processed and saved in '$OUT_DIR'"
